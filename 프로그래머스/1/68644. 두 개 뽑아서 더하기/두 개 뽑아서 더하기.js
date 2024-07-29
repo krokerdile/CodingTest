715b54b4b1854746d670c7e03a3405d7671cde61
krokerdile <39644976+krokerdile@users.noreply.github.com>
2024-07-29 23:21:38 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.99 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,18 +1,14 @@
 function solution(numbers) {
-    var answer = [];
-    let list = { };
+    let answer = []; 
     
-    numbers.map((x,idx1)=>{
-        numbers.map((y,idx2)=>{
-            if(idx1!= idx2){
-                list[x+y] = x+y;
+    let length = numbers.length;
+    for(let i=0; i<length; i++){
+        for(let j=0; j<length; j++){
+            if(i !== j){
+                answer.push(numbers[i]+numbers[j]);
             }
-        })
-    })
+        }
+    }
     
-   for(let key in list){
-       answer.push(list[key]);
-   }
-    
-    return answer;
+    return [...new Set(answer)].sort((a,b)=>a-b);
 }
