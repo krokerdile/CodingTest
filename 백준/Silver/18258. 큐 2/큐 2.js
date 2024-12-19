547268adf79d0d37b94a147f9324526392167e04
krokerdile <39644976+krokerdile@users.noreply.github.com>
2024-12-19 17:01:59 +0900

    [Silver IV] Title: 큐 2, Time: 2068 ms, Memory: 349412 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.js
@@ -0,0 +1,120 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : '../input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let count = parseInt(input[0]);
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class CustomDeque {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  addFront(item) {
+    const newNode = new Node(item);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.size++;
+  }
+
+  addRear(item) {
+    const newNode = new Node(item);
+    if (!this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.prev = this.tail;
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size++;
+  }
+
+  removeFront() {
+    if (!this.head) return null;
+    const value = this.head.value;
+    this.head = this.head.next;
+    if (this.head) {
+      this.head.prev = null;
+    } else {
+      this.tail = null;
+    }
+    this.size--;
+    return value;
+  }
+
+  removeRear() {
+    if (!this.tail) return null;
+    const value = this.tail.value;
+    this.tail = this.tail.prev;
+    if (this.tail) {
+      this.tail.next = null;
+    } else {
+      this.head = null;
+    }
+    this.size--;
+    return value;
+  }
+}
+
+const arrayDeque = new CustomDeque();
+const answer = [];
+
+for (let i = 1; i < count + 1; i++) {
+  let [order, number] = input[i].split(' ');
+  switch (order) {
+    case 'push':
+      arrayDeque.addRear(number);
+      break;
+    case 'pop':
+      if (arrayDeque.size == 0) {
+        answer.push(-1);
+      } else {
+        answer.push(arrayDeque.head.value);
+        arrayDeque.removeFront();
+      }
+      break;
+    case 'size':
+      answer.push(arrayDeque.size);
+      break;
+    case 'empty':
+      if (arrayDeque.size == 0) {
+        answer.push(1);
+      } else {
+        answer.push(0);
+      }
+      break;
+
+    case 'front':
+      if (arrayDeque.size == 0) {
+        answer.push(-1);
+      } else {
+        answer.push(arrayDeque.head.value);
+      }
+
+      break;
+    case 'back':
+      if (arrayDeque.size == 0) {
+        answer.push(-1);
+      } else {
+        answer.push(arrayDeque.tail.value);
+      }
+  }
+}
+
+console.log(answer.join('\n'));
