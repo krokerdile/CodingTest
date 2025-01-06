663e3ba865ed8a26e845af843266106bfea0bda6
krokerdile <39644976+krokerdile@users.noreply.github.com>
2025-01-06 16:06:17 +0900

    [level 2] Title: 기능개발, Time: 17.19 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.05 ms
+메모리: 39 MB, 시간: 17.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 23:39:46
+2025년 01월 06일 16:06:14
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,40 +1,29 @@
 function solution(progresses, speeds) {
-    // answer: 각 배포마다 배포되는 기능의 수가 적힌 정수 배열
     var answer = [];
-    // days: 배포일
-    let days = 1;
-    // cnt: 오늘 배포되는 기능의 수
-    let cnt = 0;
-    // progress: 현재 기능의 작업 진도
-    let progress = 0;
     
-    // 모든 작업이 다 배포될 때까지 반복
-    while(progresses[0]){
-        // 첫 번째 기능의 작업 진도
-        progress = progresses[0] + (speeds[0] * days);
-        // 첫 번째 기능의 작업 진도가 100 이상인 경우 배포 완료
-        if(progress >= 100){
-            // 배포 완료된 기능 개수 추가
-            cnt++;
-            // 배포 완료된 작업 제거
-            progresses.shift();
-            // 배포 완료된 작업의 속도 제거
-            speeds.shift();
+    let proList = progresses;
+    let speList = speeds;
+    
+    while(!proList.every(num => num >= 100)){
+        let endTask = 0; 
+        for(let i=0; i<proList.length; i++){
+            proList[i] += speList[i]; 
         }
-        // 첫 번째 기능의 작업 진도가 100 미만일 경우 배포 불가능
-        else{
-            // 배포 완료된 기능이 있는 경우, answer에 push
-            if(cnt > 0){
-                answer.push(cnt);
+        for(let i=0; i<proList.length; i++){
+            if(proList[i] >= 100){
+                endTask++;
+            }else{
+                break;
             }
-            // 배포일 증가 (다음날)
-            days++;
-            // 배포 완료된 기능 개수 초기화
-            cnt = 0;
         }
+        for(let i=0; i<endTask; i++){
+            proList.shift();
+            speList.shift();
+        }
+        
+        console.log(proList, endTask);
+        if(endTask > 0) answer.push(endTask);
     }
-    // 모든 작업이 다 배포되고 나면, 마지막으로 카운트된 배포 완료 기능 개수 push
-    answer.push(cnt);
     
     return answer;
 }
