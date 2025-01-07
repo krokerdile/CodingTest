513cc9ee16db4a32fefa4f9266a84332a04d5716
krokerdile <39644976+krokerdile@users.noreply.github.com>
2025-01-07 12:56:51 +0900

    [level 2] Title: H-Index, Time: 0.68 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,18 +1,17 @@
 function solution(citations) {
-    var answer = 0;
-
-    citations = citations.sort((a,b) => b-a);
-
-    let arr = [];
-
-    for(let i=0; i<citations.length; i++){
-        arr.push(citations[i]);
-        if(citations[i]<arr.length){
+    // 내림차순 정렬
+    citations.sort((a, b) => b - a);
+    
+    let hIndex = 0;
+    
+    for(let i = 0; i < citations.length; i++) {
+        // 현재 논문의 인용수가 논문 개수(i+1)보다 크거나 같으면
+        if(citations[i] >= i + 1) {
+            hIndex = i + 1;
+        } else {
             break;
         }
-        answer++;
     }
-
-
-    return answer;
+    
+    return hIndex;
 }
