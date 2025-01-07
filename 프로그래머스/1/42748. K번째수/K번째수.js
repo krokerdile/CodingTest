20b85691daee4d879008a693da8b1cabafd0646f
krokerdile <39644976+krokerdile@users.noreply.github.com>
2025-01-07 12:45:56 +0900

    [level 1] Title: K번째수, Time: 3.74 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,11 +1,13 @@
 function solution(array, commands) {
     var answer = [];
     
-    commands.map((command)=>{
-        let temp = array.slice(command[0]-1,command[1]);
-        temp = temp.sort((a, b) => a - b);
-        answer.push(temp[command[2]-1]);
-    })
+    for(let i=0; i<commands.length; i++){
+        
+        let temp = array.slice(commands[i][0]-1,commands[i][1]);
+        temp = temp.sort((a,b)=>a-b);
+        console.log(temp);
+        answer.push(temp[commands[i][2]-1]);
+    }
     
     return answer;
 }
