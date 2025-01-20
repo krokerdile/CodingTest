bae977f3df86ad5c397ce8965d7a9a1786eaf1d8
krokerdile <39644976+krokerdile@users.noreply.github.com>
2025-01-20 19:34:47 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 152 ms, Memory: 9660 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : '../input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let N = parseInt(input[0]);
+
+function fibonacci(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(N));
