701d8a847b8c15c837801c2d21df4e4b7cfa1bab
krokerdile <39644976+krokerdile@users.noreply.github.com>
2025-02-18 16:28:17 +0900

    [Silver III] Title: Four Squares, Time: 220 ms, Memory: 10016 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+let N = parseInt(input[0]);
+
+let dp = new Array(N + 1).fill(50001);
+
+dp[0] = 0;
+
+for (let i = 1; i <= N; i++) {
+  for (let j = 1; j * j <= i; j++) {
+    dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+  }
+}
+
+console.log(dp[N]);
