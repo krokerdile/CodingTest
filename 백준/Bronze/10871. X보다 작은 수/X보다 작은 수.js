0d8fa95bcfdd1467a60ee88366a358eb9b0fe1d3
krokerdile <39644976+krokerdile@users.noreply.github.com>
2025-03-03 23:50:23 +0900

    [Bronze V] Title: X보다 작은 수, Time: 160 ms, Memory: 12392 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -1,12 +1,16 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? 'dev/stdin' : '../input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
+const fs = require("fs");
+const file = process.platform === "linux" ? "dev/stdin" : "./text.txt";
+const input = fs.readFileSync(file).toString().trim().split("\n");
 
-let num = input[0].split(' ').map(Number);
-let list = input[1].split(' ').map(Number);
-
-list.forEach((x) => {
-  if (x < num[1]) {
-    console.log(x);
+const line1 = input[0].split(" ");
+const n = Number(line1[0]);
+const x = Number(line1[1]);
+// 값이 숫자로 인식하도록 해야됨
+let arr = input[1].split(" ");
+let result = "";
+for (i = 0; i < n; i++) {
+  if (arr[i] < x) {
+    result += arr[i] + " ";
   }
-});
+}
+console.log(result);
